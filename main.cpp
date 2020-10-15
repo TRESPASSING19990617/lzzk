f17581caff7ade5861edd6604d090a37f59e3b2d
黎张子康 <lizhangzikang@lizhangzikangdeMacBook-Pro.local>
2020-10-15 13:59:11 +0800

    main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  kangkang
+//
+//  Created by 黎张子康 on 2020/10/10.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    float x,y;
+    x=36;
+    y=sqrt(x);
+    cout<<y<<endl;
+    cout<<"康康❤️云云"<<endl;
+    return 0;
+}
